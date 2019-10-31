2f112bf4ab42e59917415617d553cfcf7ca0e46b
FaneTheEternal <artemyev.vova@gmail.com>
2019-10-31 16:26:38 +0300

    Probe 0.1

--- expansion/popup.js
+++ expansion/popup.js
@@ -1,7 +1,16 @@
 addEventListener("DOMContentLoaded", () => {
-    let checkPageButton = document.getElementById('checkPage');
+    const checkPageButton = document.getElementById('checkPage');
+    const passField = document.getElementById('field');
     checkPageButton.addEventListener('click', () => {
-        alert('Yop, hello!');
+        passField.style.visibility = '';
+        let pass = '';
+        let map = '`1234567890-=~!@#$%^&*()_+|qwertyuiop[]asdfghjkl;zxcvbnm,.QWERTYUIOP{}ASDFGHJKL:ZXCVBNM<>?';
+        for (let i = 0 ; i < 12 ; i++) {
+            pass += map[Math.floor((Math.random() * 100) % map.length)];
+            console.log(Math.floor((Math.random() * 100) % map.length));
+        }
+        console.log(pass);
+        passField.innerHTML = pass;
     });
         
 });
