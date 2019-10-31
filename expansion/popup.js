416e15c0b621b5b352435c3127605829c3b7ef89
FaneTheEternal <artemyev.vova@gmail.com>
2019-10-31 16:27:56 +0300

    some b..

--- expansion/popup.js
+++ expansion/popup.js
@@ -7,9 +7,7 @@ addEventListener("DOMContentLoaded", () => {
         let map = '`1234567890-=~!@#$%^&*()_+|qwertyuiop[]asdfghjkl;zxcvbnm,.QWERTYUIOP{}ASDFGHJKL:ZXCVBNM<>?';
         for (let i = 0 ; i < 12 ; i++) {
             pass += map[Math.floor((Math.random() * 100) % map.length)];
-            console.log(Math.floor((Math.random() * 100) % map.length));
         }
-        console.log(pass);
         passField.innerHTML = pass;
     });
         
