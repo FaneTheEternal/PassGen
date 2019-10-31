4627493c5cd5796c3f1080c1faf29ff4c2779e37
FaneTheEternal <artemyev.vova@gmail.com>
2019-10-31 15:51:45 +0300

    take basic

---
+++ expansion/popup.js
@@ -0,0 +1,7 @@
+addEventListener("DOMContentLoaded", () => {
+    let checkPageButton = document.getElementById('checkPage');
+    checkPageButton.addEventListener('click', () => {
+        alert('Yop, hello!');
+    });
+        
+});
