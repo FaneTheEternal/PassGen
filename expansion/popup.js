3cbebef1e94b6fc94bf4188ab9ebd268d220cd32
flavaboy <57231128+flavaboy@users.noreply.github.com>
2019-11-07 00:18:24 +0300

    Add files via upload
    v1.1.01

--- expansion/popup.js
+++ expansion/popup.js
@@ -1,14 +1,21 @@
 addEventListener("DOMContentLoaded", () => {
     const checkPageButton = document.getElementById('checkPage');
     const passField = document.getElementById('field');
+    const passField2 = document.getElementById('input_login');
+    const checkSaveButton = document.getElementById('DataSave');
+    
     checkPageButton.addEventListener('click', () => {
         passField.style.visibility = '';
+        passField2.style.visibility = '';
         let pass = '';
-        let map = '`1234567890-=~!@#$%^&*()_+|qwertyuiop[]asdfghjkl;zxcvbnm,.QWERTYUIOP{}ASDFGHJKL:ZXCVBNM<>?';
+        let map = '1234567890-=~!@#$%^&*()_+qwertyuiop[]asdfghjklzxcvbnmQWERTYUIOP{}ASDFGHJKLZXCVBNM<>?';
         for (let i = 0 ; i < 12 ; i++) {
             pass += map[Math.floor((Math.random() * 100) % map.length)];
         }
         passField.innerHTML = pass;
+        let pass2 = '';
+        pass2=document.getElementById('input_login').value;
+        checkSaveButton.style.visibility = '';
     });
         
 });
