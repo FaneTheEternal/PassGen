0ad900731bb061e3cc243acd0874e9920135214c
FaneTheEternal <artemyev.vova@gmail.com>
2019-11-10 21:07:15 +0300

    :)

--- expansion/popup.js
+++ expansion/popup.js
@@ -1,22 +1,22 @@
 const serverUrl = "http://192.168.1.95:3000";
 addEventListener("DOMContentLoaded", () => {
     const checkPageButton = document.getElementById('checkPage');
-    const passField = document.getElementById('field');
-    const passField2 = document.getElementById('input_login');
+    const passField = document.getElementById('input_password');
+    const loginField = document.getElementById('input_login');
     const checkSaveButton = document.getElementById('DataSave');
     const checkSeeButton = document.getElementById('Find');
 
 
     checkPageButton.addEventListener('click', () => {
         passField.style.visibility = '';
-        passField2.style.visibility = '';
+        loginField.style.visibility = '';
         let pass = '';
         let map = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&()*+,-./:;<=>?@[]^_{|}~'; 
 	    for (let i = 0 ; i < 12 ; i++) { 
 	        let b = crypto.getRandomValues( new Uint8Array(1) );
 	        pass += map[ b[0] % map.length ]; 
 	    }
-        passField.innerHTML = pass;
+        passField.value = pass;
         checkSaveButton.style.visibility = '';
         passBuffer = pass;
     });
