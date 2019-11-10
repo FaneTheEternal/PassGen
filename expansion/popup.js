bad2003ec0281455787ae8c9794948542cf83fd2
flavaboy <57231128+flavaboy@users.noreply.github.com>
2019-11-10 19:39:39 +0300

    10.11(2)

--- expansion/popup.js
+++ expansion/popup.js
@@ -3,20 +3,23 @@ addEventListener("DOMContentLoaded", () => {
     const passField = document.getElementById('field');
     const passField2 = document.getElementById('input_login');
     const checkSaveButton = document.getElementById('DataSave');
-    
+    const checkSeeButton = document.getElementById('Find');
+
+	let login = '';	
+
     checkPageButton.addEventListener('click', () => {
         passField.style.visibility = '';
         passField2.style.visibility = '';
         let pass = '';
-        let map = ' abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&\'()*+,-./:;<=>?@[\\]^_\`{|}~';
-	    for (let i = 0 ; i < 12 ; i++) { 
-	        let b = crypto.getRandomValues( new Uint8Array(1) ); 
-	        pass += map[ b[0] % map.length ]; 
-	    }
+        let map = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ! \" # $ % & \' ( ) * + , - . / : ; < = > ? @ [ \\ ] ^ _\` { | } ~'; 
+	for (let i = 0 ; i < 12 ; i++) { 
+	let [b] = crypto.getRandomValues( new Uint8Array(1) ); 
+	pass += map[ b[0] % map.length ]; 
+	}
         passField.innerHTML = pass;
-        let pass2 = '';
-        pass2=document.getElementById('input_login').value;
+        login=document.getElementById('input_login').value;
         checkSaveButton.style.visibility = '';
     });
-        
+    checkSaveButton.addEventListener('click', () => {  });
+    checkSeeButton.addEventListener('click', () => {  });
 });
