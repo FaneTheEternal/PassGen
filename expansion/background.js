aff2946a0cc37b3666a7959f76ce840af82d4cf0
FaneTheEternal <artemyev.vova@gmail.com>
2019-11-12 01:40:31 +0300

    oi takaya zhepa

--- expansion/background.js
+++ expansion/background.js
@@ -1,14 +1,11 @@
 const ip = 'localhost:8080';
 const url = `http://${ip}`;
 chrome.runtime.onMessage.addListener( (request, sender, sendResponse) => {
+  alert(request.req);
   if (request.req == 'sendS') {
     fetch(`${url}/add?login=${request.login}&password=${request.password}&domain=${request.domain}`)
             .then(r => r.text())
             .then(result => console.log(result) );
-  } else if (request.req == 'getAll') {
-    fetch(`${url}/`)
-      .then(r => r.text())
-      .then(result => sendResponse(JSON.parse(result)) );
   } else {
     fetch(`${url}/`)
       .then(r => r.text())
--- expansion/popup.js
+++ expansion/popup.js
@@ -17,7 +17,7 @@ addEventListener("DOMContentLoaded", () => {
         passField.style.visibility = '';
         loginField.style.visibility = '';
         let pass = '';
-        let map = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%()*+,-./:;<>?@[]^_{|}~'; 
+        let map = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!$*+-:;^_|~'; 
 	    for (let i = 0 ; i < 12 ; i++) { 
 	        let b = crypto.getRandomValues( new Uint8Array(1) );
 	        pass += map[ b[0] % map.length ]; 
@@ -27,34 +27,78 @@ addEventListener("DOMContentLoaded", () => {
         passBuffer = pass;
     });
 
+
     checkSaveButton.addEventListener('click', () => {
+        console.log('save');
+        
         const login = document.getElementById('input_login').value;
         const pass = document.getElementById('input_password').value;
         checkSaveButton.innerHTML ="Login details saved";
         //void chrome.runtime.lastError;
-        chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        /*chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
             chrome.tabs.sendMessage(tabs[0].id, {login: login, password: pass, req: 'send'}, response => {
+                console.log(`got ${response}`);
+                
+                void chrome.runtime.lastError;
+            });
+        });*/
+
+        // get domain
+        let domain = '';
+        chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+            chrome.tabs.sendMessage(tabs[0].id, { req: 'getDomain'}, response => {
+                domain = response;
+                fetch(`http://localhost:8080/add?login=${login}&password=${pass}&domain=${domain}`)
+                    .then(r => r.text())
+                    .then(result => console.log(result) );
                 void chrome.runtime.lastError;
             });
         });
-    check.allButton.addEventListener('click', () => {
-	currs.style.display = "none";
-	allView.style.display = "block";
-         });
     });
+
+    
 
     checkSeeButton.addEventListener('click', () => { 
         HIDE.style.display = "none"; 
         view.style.display = "block"; 
         const field = document.getElementById('currentfield');
+
+        // get domain
+        let domain;
+        chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+            chrome.tabs.sendMessage(tabs[0].id, { req: 'getDomain'}, response => {
+                console.log(response);
+                domain = response;
+                fetch(`http://localhost:8080/`)
+                    .then(r => r.text())
+                    .then(result => {
+                        const data = JSON.parse(result);        
+                        for (let login in data[domain]) {
+                            field.innerHTML += `<div id="dataStr">${domain}: ${login} : ${data[domain][login]}</div>`;
+                        }
+                });
+                void chrome.runtime.lastError;
+            });
+        });
+
+        
+    });
+
+
+    allButton.addEventListener('click', () => {
+        const fieldOld = document.getElementById('currentfield');
+        fieldOld.style.display = 'none';
+        const field = document.getElementById('allfields');
+        field.style.display = 'block';
+        field.innerHTML = '';
+
         fetch(`http://localhost:8080/`)
             .then(r => r.text())
             .then(result => {
                 const data = JSON.parse(result);
-                
                 for (let domain in data) {
                     for (let login in data[domain]) {
-                        field.innerHTML += `<div id="dataStr">${domain}: ${login} : ${data[domain][login]}</div>`;
+                        field.innerHTML += `<div class="dataStr">${domain}: ${login} : ${data[domain][login]}</div>`;
                     }
                 }
             });
