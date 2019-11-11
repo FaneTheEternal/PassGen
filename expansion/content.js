81806549ca47b532e2a27e92639f349118d7b685
FaneTheEternal <artemyev.vova@gmail.com>
2019-11-11 22:46:03 +0300

    todo

--- expansion/content.js
+++ expansion/content.js
@@ -3,3 +3,7 @@ const x = (window.location.hostname.toString());
 chrome.runtime.onMessage.addListener( (request, sender, sendResponse) => {
     chrome.runtime.sendMessage({login: request.login, password: request.password, domain: x, req: 'send'});
   });
+
+const login = document.getElementsByName('email') ? 
+document.getElementsByName('email') : (document.getElementsByName('login') ? document.getElementsByName('login') : document.getElementsByName('username'));
+const password = $("type=password");
